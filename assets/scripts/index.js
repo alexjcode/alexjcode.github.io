1a00a43c02f9a24c5acfbf60fc781a1d6cbeb3e3
alexjcode <alexjgraphics@gmail.com>
2019-09-09 15:43:21 -0400

    Skills header now changes on hover

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,19 @@
-// $(() => {
-//   //
-// })
+const onSkillHover = event => {
+  event.preventDefault()
+  const icon = $(event.target)
+  $('.skills-head').text(icon.attr('alt'))
+}
+
+const onSkillLeave = event => {
+  $('.skills-head').text('Skills')
+}
+
+$(() => {
+  // $('.icon-socketio').innerHTML('')
+  // $('.skills').on('mouseenter', '.icon', () => {})
+  // $('.skills').on('mouseleave', '.icon', () => {})
+  $('.icon').on({
+    mouseenter: onSkillHover,
+    mouseleave: onSkillLeave
+  })
+})

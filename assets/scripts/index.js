08b00b50b317a1253ef9f743fce65c35ed500397
alexjcode <alexjgraphics@gmail.com>
2019-09-09 19:07:36 -0400

    Smoother skill header updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,19 +1,37 @@
-const onSkillHover = event => {
-  event.preventDefault()
-  const icon = $(event.target)
-  $('.skills-head').text(icon.attr('alt'))
-}
+let old = ''
+
+// const onSkillHover = event => {
+//   event.preventDefault()
+//   const icon = $(event.target)
+//   const alt = icon.attr('alt')
+//   if (alt !== 'skills' && alt) {
+//     old = alt
+//   }
+//   $('.skills-head').text(old)
+// }
 
 const onSkillLeave = event => {
-  $('.skills-head').text('Skills')
+  $('.skills-head').text('')
+  $('.skills-head').hide()
 }
 
 $(() => {
   // $('.icon-socketio').innerHTML('')
   // $('.skills').on('mouseenter', '.icon', () => {})
   // $('.skills').on('mouseleave', '.icon', () => {})
-  $('.icon').on({
-    mouseenter: onSkillHover,
+  $('.skills').on('mouseover', (ev) => {
+    event.preventDefault()
+    const icon = $(event.target)
+    const alt = icon.attr('alt')
+    if (alt !== 'skills' && alt) {
+      old = alt
+    }
+    $('.skills-head').text(old)
+    $('.skills-head').show()
+  })
+  $('.skill-row').on({
     mouseleave: onSkillLeave
+    // mouseenter: ev => setTimeout(onSkillHover(ev), 0),
+    // mouseleave: ev => setTimeout(onSkillLeave(ev), 0)
   })
 })
